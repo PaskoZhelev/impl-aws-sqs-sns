caa7efff96e1101bba6f5aeb2af6775923b3eca1
zhelepas <pasko.zhelev@diconium.com>
2024-04-19 16:25:49 +0200

    initial commit

---
+++ src/main/java/com/mnp/implawssqssns/ImplAwsSqsSnsApplication.java
@@ -0,0 +1,13 @@
+package com.mnp.implawssqssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplAwsSqsSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplAwsSqsSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnp/implawssqssns/ImplAwsSqsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnp.implawssqssns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplAwsSqsSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
